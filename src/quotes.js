c6bc55aa57fcc701042340d8846d0264f2418430
Shreena Mani <167695614+ItsShreena@users.noreply.github.com>
2026-05-20 23:35:34 +0530

    Add files via upload

---
+++ src/quotes.js
@@ -0,0 +1,14 @@
+const quotes = [
+  "Small progress is still progress.",
+  "Consistency beats motivation.",
+  "Discipline creates freedom.",
+  "Focus on becoming better.",
+  "Done is better than perfect.",
+  "One task at a time.",
+  "Your future self will thank you.",
+  "Stay patient and trust the process.",
+  "Success comes from daily habits.",
+  "Tiny steps lead to big results."
+]
+
+export default quotes
---
+++ src/rewards.js
@@ -0,0 +1,38 @@
+const rewards = [
+  {
+    id: 1,
+    name: "Blue Theme",
+    levelRequired: 2,
+    icon: "💙",
+  },
+
+  {
+    id: 2,
+    name: "Cool Sunglasses",
+    levelRequired: 3,
+    icon: "🕶️",
+  },
+
+  {
+    id: 3,
+    name: "Golden Pet",
+    levelRequired: 5,
+    icon: "✨",
+  },
+
+  {
+    id: 4,
+    name: "Space Background",
+    levelRequired: 7,
+    icon: "🚀",
+  },
+
+  {
+    id: 5,
+    name: "Legend Badge",
+    levelRequired: 10,
+    icon: "🏆",
+  },
+]
+
+export default rewards
